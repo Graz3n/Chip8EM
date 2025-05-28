0691c70ab19c6906168553962aa45b2b8680543f
Graz3n <gjaime124@gmail.com>
2025-05-28 04:02:32 -0700

    Add files via upload

--- chip8.cpp
+++ chip8.cpp
@@ -1,39 +1,48 @@
-#include <iostream>
 #include "chip8.h"
+#include <random>
 #include <string>
 #include <fstream>
 #include <cstdint>
+#include <iostream>
 void chip8::initialize(){
 pc = 0x200;
 opcode = 0;
 I = 0;
 sp = 0;
-
+drawFlag = false;
 // reset the stack and registers 
 	for(int i = 0; i < 16; ++i){
 		stack[i] = 0;	
 		V[i] = 0;		
 	}
 // reset graphics display
-	for(int i = 0; i < sizeof(gfx) / sizeof(gfx[0]) ; ++i){
+	for(int i = 0; i < sizeof(gfx); ++i){
 		gfx[i] = 0;		
 	}
 //clear memory
-	for(int i = 0; i < sizeof(memory) / sizeof(memory[0]); ++i){
+	for(int i = 0; i < sizeof(memory); ++i){
 		memory[i] = 0;
 	}
 
 // load in font
 	for(int i = 0; i < 80; ++i){
+		memory[i] = chip8_fontset[i];
 	}
 // reset timers
+	delay_timer = 60;
+	sound_timer = 60;
 }
 void chip8::loadGame(const std::string& filename){
 	std::ifstream file(filename, std::ios::binary | std::ios::ate);
 	//read buffer
-	std::filebuf* pbuf = file.rdbuf();
+	if (!file.is_open()) {
+    std::cerr << "Failed to open file: " << filename << std::endl;
+		return;
+		}
+
 	//get buffer size and buffer pointer to array char
 	std::streamsize size = file.tellg();
+	char* buffer = new char[size];
 
 	if(size > 3584){
 	std::cerr << "file is too large";
@@ -42,7 +51,6 @@ void chip8::loadGame(const std::string& filename){
 	} 
 	file.seekg(0,std::ios::beg);
 
-	char* buffer = new char[size];
 
 	file.read(buffer,size);	
 	file.close();
@@ -58,7 +66,7 @@ void chip8::loadGame(const std::string& filename){
 void chip8::emulateCycle(){
 //fetch some opcode:
 	opcode = memory[pc] << 8 | memory[pc + 1]; 
-
+	std::cout << "PC: " << std::hex << pc << "  Opcode: " << opcode << std::endl;
 	switch(opcode & 0xF000) {
 		case 0xA000:
 			I = opcode & 0x0FFF;
@@ -69,12 +77,12 @@ void chip8::emulateCycle(){
 				//go back
 				pc = stack[sp - 1];
 				sp--;
-				pc = pc + 2;
+				pc += 2;
 				break;
 
 			}else{
 
-			for(int i = 0; i < sizeof(gfx) / sizeof(gfx[0]) ; ++i){
+			for(int i = 0; i < sizeof(gfx); ++i){
 				gfx[i] = 0;		
 			}
 				pc = pc + 2;
@@ -90,14 +98,14 @@ void chip8::emulateCycle(){
 			pc = opcode & 0x0FFF;		
 			break;
 		case 0x3000:
-			if(V[(opcode & 0x0F00) >> 8] == opcode & 0x00FF){
+			if((V[(opcode & 0x0F00) >> 8]) == (opcode & 0x00FF)){
 				pc = pc + 4;
 			}else{
 				pc = pc + 2;
 			}
 		break;
 		case 0x4000:
-				if(V[(opcode & 0x0F00) >> 8] != opcode & 0x00FF){
+				if((V[(opcode & 0x0F00) >> 8]) != (opcode & 0x00FF)){
 				pc = pc + 4;
 			}else{
 				pc = pc + 2;
@@ -112,82 +120,248 @@ void chip8::emulateCycle(){
 		break;
 		case 0x6000:
 			V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
+			pc += 2;
 		break;
 		case 0x7000: 
-			V[(opcode & 0x0F00) >> 8] =  V[(opcode & 0x0F00) >> 8] + opcode & 0x00FF; 
+			V[(opcode & 0x0F00) >> 8] += (opcode & 0x00FF); 
+			pc += 2;
 		break;
 
 		case 0x8000:
-
 		switch(opcode & 0x000F){
 			case 0x0000:
-			V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4];	
+			V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];	
+			pc += 2;
 			break;
 			case 0x0001:
-			char X = V[(opcode & 0x0F00) >> 8];
-			char Y = V[(opcode & 0x00F0) >> 4];
-			V[X] = X | Y;
+			V[(opcode & 0x0F00) >> 8] = (V[(opcode & 0x0F00) >> 8])|(V[(opcode & 0x00F0) >> 4]);
+			pc += 2;
 			break;
-			case 0x0002:
-			char X = V[(opcode & 0x0F00) >> 8];
-			char Y = V[(opcode & 0x00F0) >> 4];
-			V[X] = X & Y;
+			case 0x0002: 
+			V[(opcode & 0x0F00) >> 8]= V[(opcode & 0x0F00) >> 8] & V[(opcode & 0x00F0) >> 4];
+			pc += 2;
 			break;
-			case 0x0003:
-			char X = V[(opcode & 0x0F00) >> 8];
-			char Y = V[(opcode & 0x00F0) >> 4];
-			V[X] = X ^ Y;
+				
+			case 0x0003: 
+			V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] ^ V[(opcode & 0x00F0) >> 4];
+			pc += 2;
 			break;
-			case 0x0004:
-			char X = V[(opcode & 0x0F00) >> 8];
-			char Y = V[(opcode & 0x00F0) >> 4];
-			uint8_t sum = Y + X; 
+				
+			case 0x0004:{
+			uint16_t sum = V[(opcode & 0x00F0) >> 4] + V[(opcode & 0x0F00) >> 8]; 
 			V[0xF] = (sum > 255) ? 1 : 0;
+			V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F0) >> 4];
+			pc += 2;
 			break;
-			case 0x0005:
-			char X = V[(opcode & 0x0F00) >> 8];
-			char Y = V[(opcode & 0x00F0) >> 4];
+			} 
+			case 0x0005: 
 
-
-			V[0xF] = (V[X] >= V[Y]) ? 1 : 0; 
+			V[0xF] = (V[(opcode & 0x0F00) >> 8] >= V[(opcode & 0x00F0) >> 4]) ? 1 : 0; 
+			V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
+			pc += 2;
 			break;
-			case 0x0006:
-			char X = (V[(opcode & 0x0F00) >> 8]);
-			uint16_t lsb = X & 0x1;
+				
+			case 0x0006:{
+			uint16_t lsb = V[(opcode & 0x0F00) >> 8] & 0x1;
 
 
 			V[0xF] = lsb;
-			V[(opcode & 0x0F00) >> 8] =  X >> 1; 
+			V[(opcode & 0x0F00) >> 8] =  V[(opcode & 0x0F00) >> 8] >> 1; 
+
+			pc += 2;
 			break;
-			case 0x0007:
-			char X = V[(opcode & 0x0F00) >> 8];
-			char Y = V[(opcode & 0x00F0) >> 4];
-			V[0xF] = (V[X] >= V[Y]) ? 1 : 0;
-			V[X] = Y - X;
+				} 
+			
+			
+			case 0x0007: 
+			V[0xF] = (V[(opcode & 0x00F0) >> 4]>= V[(opcode & 0x0F00) >> 8]) ? 1 : 0;
+			V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] - V[(opcode & 0x00F0) >> 4];
+			pc += 2;
 			break;
-			case 0x000E:
-			char X = (V[(opcode & 0x0F00) >> 8]);
-			uint16_t msb = (X & 0x80) >> 7;
+				
+			case 0x000E:{
+			uint16_t msb = (V[(opcode & 0x0F00) >> 8] & 0x80) >> 7;
 			V[0xF] = msb;
-			V[(opcode & 0x0F00) >> 8] =  X << 1;
+			V[(opcode & 0x0F00) >> 8] =  (V[(opcode & 0x0F00) >> 8]) << 1;
+			pc += 2;
 			break;
-			default:
-			std::cout << "Couldn't identify opcode" << "\n";
-			break;
-		}
-		case 0x9000:
+				}
+			default: 
+					
+				printf("bad logic switch");	
+				std::cout << "Couldn't identify opcode" << "\n";
+				pc +=2;
+				break;
+					
+	}
+		break;
+		case 0x9000: {
 		if(V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4]){
 			pc = pc + 4;
 		}else{
 			pc = pc + 2;
 		}
+			break;
+				}
+	
+		case 0xB000: {
+			pc = V[0] + (opcode & 0x0FFF);
+			pc += 2;
+			break;
+			}
+		case 0xC000:{
+			int random;
+			std::random_device rd;
+			std::mt19937 gen(rd());
+			std::uniform_int_distribution<> distr(0, 255);
+			random = distr(gen);
+			
+			
+			V[(opcode & 0x0F00) >> 8]  = (opcode & 0x00FF) & random;
+			pc += 2;
+
+			break;
+		}
+		case 0xD000: {
+  			   unsigned short x = V[(opcode & 0x0F00) >> 8];
+			   unsigned short y = V[(opcode & 0x00F0) >> 4];
+			   unsigned short height = opcode & 0x000F;
+			   unsigned short pixel;
+
+			   V[0xF] = 0;
+			   for (int yline = 0; yline < height; yline++)
+			  {
+			     pixel = memory[I + yline];
+			     for(int xline = 0; xline < 8; xline++)
+			     {
+			       if((pixel & (0x80 >> xline)) != 0)
+			       {
+			         if(gfx[(x + xline + ((y + yline) * 64))] == 1)
+			           V[0xF] = 1;
+			         gfx[x + xline + ((y + yline) * 64)] ^= 1;
+			       }
+			     }
+			   }
+
+			 drawFlag = true;
+			 pc += 2;
+			 break;
+			 }
+		case 0xE000:
+			switch(opcode & 0x00F0){
+				case 0x0090:{
+					if(key[V[(opcode & 0x0F00) >> 8]] != 0)
+						pc += 4;
+					else
+						pc+= 2;
+					break;
+				}
+				case 0x00A0:{
+					if(key[V[(opcode & 0x0F00) >> 8]] == 0)
+						pc += 4;
+					else
+						pc+= 2;
+					break;
+				}
+				default: {
+				printf("bad E switch");	
+				std::cout << "Couldn't identify opcode" << "\n";
+					break;
+				}
+			} 
+		case 0xF000:
+			switch(opcode & 0x00FF){
+				case 0x0007:{
+					V[(opcode & 0x0F00) >> 8] = delay_timer;
+					pc += 2;
+					break;
+				}
+				case 0x000A: {
+ 					   uint8_t x = (opcode & 0x0F00) >> 8;
+    						bool keyPressed = false;
+
+  					  for (int i = 0; i < 16; ++i) {
+       							 if (key[i]) {
+           							 V[x] = i;
+            							keyPressed = true;
+          								  break;
+      								  }
+   								 }
+
+    						if (!keyPressed)
+       						 return; // Skip pc += 2, wait for key
+   						 pc += 2;
+    						break;
+				}
+				case 0x0015:{
+					delay_timer = V[(opcode & 0x0F00) >> 8];
+					pc += 2;
+					break;
+				}
+				case 0x0018:{
+					sound_timer = V[(opcode & 0x0F00) >> 8];
+					pc += 2;
+					break;
+				}
+				case 0x001E: {
+					I += V[(opcode & 0x0F00) >> 8];
+					pc += 2;
+					break;
+				}
+				case 0x0029: {
+					uint8_t Vx = (opcode & 0x0F00) >> 8;
+					uint8_t digit = V[Vx];
+					I = 0x50 + (5 * digit);
+					pc += 2;
+				}
+				case 0x0033: {
+					uint8_t value = V[(opcode & 0x0F00) >> 8];
+
+					memory[I] = value/100;
+					memory[I + 1] = (value/10) % 10;
+					memory[I + 2] = value % 10;
+					pc += 2;
+					break;
+				}
+				case 0x0055:{
+					for(int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i){
+						memory[I + i] = V[i];
+					}
+					I += (ushort) (((opcode & 0x0F00) >> 8) + 1);
+					pc += 2;
+					break;
+				}
+				case 0x0065:{
+					for(int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i){
+						V[i] = memory[I + i];
+					}
+					I += (ushort)(((opcode % 0x0F00) >> 8) + 1);
+					pc += 2;
+					break;	
+				}
+				default: {
+				printf("opcode bad LAST");			
+				std::cout << "Couldn't identify opcode" << "\n";
+					break;
+				}
+
+			}
 	}
+	//update timer
+	if(delay_timer > 0){
+	--delay_timer;
 
+	}
+	if(sound_timer > 0){
+		if(sound_timer == 1){
+			printf("BEEP /n");
+		}
+	 --sound_timer;
+	}
 }
-
-
-
-
-
-
+void chip8::clearScreen(){
+	for(int i = 0; i < sizeof(gfx); ++i){
+		gfx[i] = 0;		
+	}
+}
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,188 @@
-
-
+#include <chrono>
+#include <SDL2/SDL.h>
 #include "chip8.h" // Your cpu core implementation
 chip8 myChip8;
+void drawGraphics(uint8_t* gfx);
+void setupGraphics();
+void setKeys();
+const int VIDEO_WIDTH = 64;
+const int VIDEO_HEIGHT = 32;
+const int PIXEL_SIZE = 10;
+
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+
 int main(int argc, char **argv) {
+	const int cycleDelay = 1000/60;
+	setupGraphics();
+	myChip8.initialize();
+	myChip8.loadGame("CHIP8GAMES/pumpkindressup.ch8");
+	auto lastCycleTime = std::chrono::high_resolution_clock::now(); 
+	for(;;){
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
+		if(dt > cycleDelay){
+		lastCycleTime = currentTime;
 
+		myChip8.emulateCycle();
+			uint8_t* graphicsP = myChip8.gfx;
+		if(myChip8.drawFlag){
+			drawGraphics(graphicsP);
+			myChip8.drawFlag = false;
+			}
+			
+		setKeys();
+		}
 
-
+		}
+	return 0;
  }
+
+void setupGraphics(){ 
+	SDL_Init(SDL_INIT_VIDEO);
+	window = SDL_CreateWindow("Chip-8 emulator",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, VIDEO_WIDTH * PIXEL_SIZE, VIDEO_HEIGHT * PIXEL_SIZE, SDL_WINDOW_SHOWN);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
+void drawGraphics(uint8_t* gfx){
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // black background
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // white pixels
+
+    for (int y = 0; y < VIDEO_HEIGHT; ++y) {
+        for (int x = 0; x < VIDEO_WIDTH; ++x) {
+            if (gfx[y * VIDEO_WIDTH + x]) {
+                SDL_Rect pixel = {
+                    x * PIXEL_SIZE,
+                    y * PIXEL_SIZE,
+                    PIXEL_SIZE,
+                    PIXEL_SIZE
+                };
+                SDL_RenderFillRect(renderer, &pixel);
+            }
+        }
+    }
+
+    SDL_RenderPresent(renderer);
+
+}
+void setKeys(){
+	SDL_Event e;
+	while(SDL_PollEvent(&e)){
+		switch(e.type){
+			case SDL_KEYDOWN:{
+				switch(e.key.keysym.sym){
+			case SDLK_1:
+			myChip8.key[0] = 1; 
+			break;
+			case SDLK_2:
+			myChip8.key[1] = 1; 
+			break;
+			case SDLK_3:
+			myChip8.key[2] = 1; 
+			break;
+			case SDLK_4:
+			myChip8.key[3] = 1; 
+			break;
+			case SDLK_q:
+			myChip8.key[4] = 1; 
+			break;
+			case SDLK_w:
+			myChip8.key[5] = 1; 
+			break;
+			case SDLK_e:
+			myChip8.key[6] = 1; 
+			break;
+			case SDLK_r:
+			myChip8.key[7] = 1; 
+			break;
+			case SDLK_a:
+			myChip8.key[8] = 1; 
+			break;
+			case SDLK_s:
+			myChip8.key[9] = 1; 
+			break;
+			case SDLK_d:
+			myChip8.key[10] = 1; 
+			break;
+			case SDLK_f:
+			myChip8.key[11] = 1; 
+			break;
+			case SDLK_z:
+			myChip8.key[12] = 1; 
+			break;
+			case SDLK_x:
+			myChip8.key[13] = 1; 
+			break;
+			case SDLK_c:
+			myChip8.key[14] = 1; 
+			break;
+			case SDLK_v:
+			myChip8.key[15] = 1; 
+			break;
+				}
+
+			}
+			break;
+			case SDL_KEYUP:{
+				switch(e.key.keysym.sym){
+			case SDLK_1:
+			myChip8.key[0] = 0; 
+			break;
+			case SDLK_2:
+			myChip8.key[1] = 0; 
+			break;
+			case SDLK_3:
+			myChip8.key[2] = 0; 
+			break;
+			case SDLK_4:
+			myChip8.key[3] = 0; 
+			break;
+			case SDLK_q:
+			myChip8.key[4] = 0; 
+			break;
+			case SDLK_w:
+			myChip8.key[5] = 0; 
+			break;
+			case SDLK_e:
+			myChip8.key[6] = 0; 
+			break;
+			case SDLK_r:
+			myChip8.key[7] = 0; 
+			break;
+			case SDLK_a:
+			myChip8.key[8] = 0; 
+			break;
+			case SDLK_s:
+			myChip8.key[9] = 0; 
+			break;
+			case SDLK_d:
+			myChip8.key[10] = 0; 
+			break;
+			case SDLK_f:
+			myChip8.key[11] = 0; 
+			break;
+			case SDLK_z:
+			myChip8.key[12] = 0; 
+			break;
+			case SDLK_x:
+			myChip8.key[13] = 0; 
+			break;
+			case SDLK_c:
+			myChip8.key[14] = 0; 
+			break;
+			case SDLK_v:
+			myChip8.key[15] = 0; 
+			break;
+
+
+				}
+			}
+
+		}
+		
+	}
+
+}
 
 
